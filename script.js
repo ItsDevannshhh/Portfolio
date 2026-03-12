f66202de4c899247b4a798281f4816684780797c
ItsDevannshhh <gamerfrost125@gmail.com>
2026-03-12 13:35:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// SMOOTH SCROLL
+
+document.querySelectorAll('nav a').forEach(anchor => {
+
+    anchor.addEventListener('click', function (e) {
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
